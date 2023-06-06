b1edca5111c027a846b027f9889cc541fed1e417
Laurent Chau <mplaurent.chau@gmail.com>
2023-06-06 18:07:47 +0200

    build: project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,16 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        red: 'hsla(0, 97%, 63%, 1)',
+        blue: {
+          semi: 'hsla(223, 36%, 13%, 1)',
+          dark: 'hsla(224, 30%, 9%, 1)',
+          greyish: 'hsla(223, 23%, 46%, 1)',
+        },
+        white: 'hsla(0, 0%, 100%, 1)',
+      },
     },
   },
   plugins: [],
-}
+};
